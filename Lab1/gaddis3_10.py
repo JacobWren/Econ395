fd337c0d5c51dde64fe2569d26a87bc7c04e24d1
Jacob Wren <46506182+JacobWren@users.noreply.github.com>
2020-12-09 08:12:06 -0800

    Update gaddis3_10.py

--- Lab1/gaddis3_10.py
+++ Lab1/gaddis3_10.py
@@ -8,17 +8,18 @@ QUARTER_VALUE = .25
 
 
 def makes_a_dollar(num_pennies, num_nickels, num_dimes, num_quarters):
-    #add up value for each coin
+    # add up value for each coin
     penny_value = PENNY_VALUE * num_pennies
     nickel_value = NICKEL_VALUE * num_nickels
     dime_value = DIME_VALUE * num_dimes
     quarter_value = QUARTER_VALUE * num_quarters
     total_cent_value = quarter_value + dime_value + nickel_value + penny_value
-    #return total_cent_value
-    #boolean expression!
+    # return total_cent_value
+    # boolean expression!
     if  total_cent_value == 1:
         return True
     else:
         return False
+    
 #print(makes_a_dollar(0,0,0,4))
     
