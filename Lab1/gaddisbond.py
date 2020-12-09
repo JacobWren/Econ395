b48904b04af419eb3c50d41ad90eb46cd061f045
Jacob Wren <46506182+JacobWren@users.noreply.github.com>
2020-12-09 08:14:52 -0800

    Update gaddisbond.py

--- Lab1/gaddisbond.py
+++ Lab1/gaddisbond.py
@@ -2,16 +2,12 @@
 
 def bond_value(C, i, M, N):
         PV_value_of_coupons = 0 # have to define this in the function
-#because we have an assignment statement below. When you have an assignment
-#statement in a function it ignores any variables defined outside of the
-#function.
+# because we have an assignment statement below. When you have an assignment
+# statement in a function it ignores any variables defined outside of the
+# function.
         for j in range(1,N+1):
-                #this is an assignment statement
+                # this is an assignment statement
                 PV_value_of_coupons = PV_value_of_coupons + C/(1+i)**j
-                #print(PV_value_of_coupons)
-        PV_face_value = M/(1+i)**N #this is outside the for loop, only 1 coupon
+        PV_face_value = M/(1+i)**N # this is outside the for loop, only 1 coupon
         Price = round(PV_value_of_coupons + PV_face_value,2)
         return Price
-
-      
-	
